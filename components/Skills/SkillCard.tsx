0bb96215c0f02911876fb3c52e9d744933025d6b
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-09-04 00:24:23 +0330

    Add links to cards

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -3,23 +3,26 @@ import Image from "next/image";
 interface Props {
   title: string;
   imageName: string;
+  link: string;
 }
 
-const SkillCard: React.FC<Props> = ({ title, imageName }) => {
+const SkillCard: React.FC<Props> = ({ title, imageName, link }) => {
   return (
-    <div>
-      <div className="group flex flex-col items-center justify-between h-24 w-24 m-auto p-3 card">
-        <Image
-          src={`/assets/logos/${imageName}.svg`}
-          alt={title}
-          width={40}
-          height={40}
-        />
-        <span className="text-xs text-zinc-200 group-hover:text-zinc-100">
-          {title}
-        </span>
-      </div>
-    </div>
+    <a
+      href={link}
+      target="_blank"
+      className="group flex flex-col items-center justify-between h-24 w-32 m-auto p-3 card"
+    >
+      <Image
+        src={`/assets/logos/${imageName}.svg`}
+        alt={title}
+        width={40}
+        height={40}
+      />
+      <span className="text-base text-zinc-200 group-hover:text-zinc-100">
+        {title}
+      </span>
+    </a>
   );
 };
 
--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -2,32 +2,60 @@ import SectionTitle from "../common/SectionTitle";
 import SkillCard from "./SkillCard";
 
 const skills = [
-  { title: "HTML5", imageName: "html" },
-  { title: "CSS3", imageName: "css" },
-  { title: "JavaScript", imageName: "javascript" },
-  { title: "TypeScript", imageName: "typescript" },
-  { title: "React.js", imageName: "react" },
-  { title: "Next.js", imageName: "next" },
-  { title: "Bootstrap", imageName: "bootstrap" },
-  { title: "Tailwind CSS", imageName: "tailwind" },
-  { title: "Material UI", imageName: "mui" },
-  { title: "Axios", imageName: "axios" },
-  { title: "Git", imageName: "git" },
-  { title: "React Router", imageName: "react router" },
-  { title: "Redux", imageName: "redux" },
+  {
+    title: "HTML5",
+    imageName: "html",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    title: "CSS3",
+    imageName: "css",
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    title: "JavaScript",
+    imageName: "javascript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    title: "TypeScript",
+    imageName: "typescript",
+    link: "https://www.typescriptlang.org/",
+  },
+  { title: "React.js", imageName: "react", link: "https://react.dev/" },
+  { title: "Next.js", imageName: "next", link: "https://nextjs.org/" },
+  {
+    title: "React Router",
+    imageName: "react router",
+    link: "https://reactrouter.com/en/main",
+  },
+  {
+    title: "Redux Toolkit",
+    imageName: "redux",
+    link: "https://redux-toolkit.js.org/",
+  },
+  {
+    title: "Bootstrap",
+    imageName: "bootstrap",
+    link: "https://getbootstrap.com/",
+  },
+  {
+    title: "Tailwind CSS",
+    imageName: "tailwind",
+    link: "https://tailwindcss.com/",
+  },
+  { title: "Material UI", imageName: "mui", link: "https://mui.com/" },
+  { title: "Axios", imageName: "axios", link: "https://axios-http.com/" },
+  { title: "Git", imageName: "git", link: "https://git-scm.com/" },
 ];
 
 const Skills = () => {
   return (
     <section className="container py-8">
       <SectionTitle title="Skills" />
-      <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-4">
         {skills.map((skill, index) => (
-          <SkillCard
-            key={index}
-            title={skill.title}
-            imageName={skill.imageName}
-          />
+          <SkillCard key={index} {...skill} />
         ))}
       </div>
     </section>
