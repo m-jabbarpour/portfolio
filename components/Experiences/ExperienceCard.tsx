7950d82bfe46af30977a6403b12153cc25ef675d
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-09-04 00:23:47 +0330

    Develop the Experiences Section

--- components/Experiences/ExperienceCard.tsx
+++ components/Experiences/ExperienceCard.tsx
@@ -1,5 +1,50 @@
-const ExperienceCard = () => {
-  return <div className="card"></div>;
+import Image from "next/image";
+
+interface Props {
+  role: string;
+  company: string;
+  date: string;
+  logoName: string;
+  link: string;
+  achievements: string[];
+}
+
+const ExperienceCard: React.FC<Props> = ({
+  role,
+  company,
+  date,
+  logoName,
+  link,
+  achievements,
+}) => {
+  return (
+    <a href={link} target="_blank" className="group card py-4 px-5">
+      <div className="flex gap-2 mb-4">
+        <Image
+          src={`/assets/logos/${logoName}`}
+          alt="IT Shams"
+          width={60}
+          height={60}
+        />
+
+        <div className="flex flex-col justify-around">
+          <h6 className="text-zinc-200 group-hover:text-zinc-100">{role}</h6>
+          <h6 className="text-zinc-200 group-hover:text-zinc-100">{company}</h6>
+          <span className="text-zinc-400 group-hover:text-zinc-300 transition-all duration-300">
+            {date}
+          </span>
+        </div>
+      </div>
+
+      <ul className="list-disc text-zinc-200 group-hover:text-zinc-100 pl-2">
+        {achievements.map((achievement, index) => (
+          <li key={index} className="mb-2">
+            {achievement}
+          </li>
+        ))}
+      </ul>
+    </a>
+  );
 };
 
 export default ExperienceCard;
--- components/Experiences/index.tsx
+++ components/Experiences/index.tsx
@@ -1,10 +1,42 @@
 import SectionTitle from "../common/SectionTitle";
+import ExperienceCard from "./ExperienceCard";
+
+const experiences = [
+  {
+    role: "Front-End Developer",
+    company: "IT Shams",
+    logoName: "itshams.svg",
+    date: "Oct. 2022 - Present",
+    link: "https://www.itshams.ir/",
+    achievements: [
+      " Working on an enterprise Suggestion System web application (React.js) for large companies and organizations, including Iran Khodro Co. (with 60k+ users) and Tabriz Municipality (with 10k+ users)",
+      "Worked with Material UI (for user interface), AG Grid and High-Charts (for representation of large data), SignalR (for web socket connections), Axios (for Rest API connections), Context API (for state management), and React Hook Form and Yup (for form handling and validation)",
+      "Implemented reusable components and custom hooks, resulting in asignificant reduction of over 90% in redundant code and a streamlined development process, resulting in a time-saving of over 20%.",
+      "Refactored the code base according to the best practices and design patterns, improving the performance by reducing unnecessary rerenders by more than 80%.",
+      "Communicated with the technical lead and the back-end development team to create a user-friendly UI and UX",
+    ],
+  },
+  {
+    role: "Front-End Developer Intern",
+    company: "Maktab Sharif",
+    logoName: "maktab sharif.png",
+    date: "Feb. 2022, Sep. 2022",
+    link: "https://maktabsharif.ir/",
+    achievements: [
+      "Developed a number of small and large projects, using a variety of Front-End development technologies, from basic ones (HTML, CSS, and vanilla JavaScript) to modern ones (React.js, Next.js)",
+    ],
+  },
+];
 
 const Experiences = () => {
   return (
     <section className="container py-8">
       <SectionTitle title="Experiences" />
-      
+      <div className="flex flex-col gap-6">
+        {experiences.map((experience) => (
+          <ExperienceCard key={experience.date} {...experience} />
+        ))}
+      </div>
     </section>
   );
 };
