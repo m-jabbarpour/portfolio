00867867517ccea390c5f780acf5ee82dc2bbe2d
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-09-04 00:23:04 +0330

    Modify styles

--- components/common/SectionTitle.tsx
+++ components/common/SectionTitle.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const SectionTitle: React.FC<Props> = ({ title }) => {
   return (
-    <h5 className="text-zinc-200 text-base lg:text-4xl text-center font-bold">
+    <h5 className="text-zinc-200 text-center font-bold mb-8">
       {title}
     </h5>
   );
